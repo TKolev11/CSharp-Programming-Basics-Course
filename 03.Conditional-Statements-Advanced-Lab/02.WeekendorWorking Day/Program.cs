dae0f0336d7435d62dd07aaec68953fb1eaabc3a
TKolev11 <94370720+TKolev11@users.noreply.github.com>
2024-01-19 16:01:03 +0200

    1st commit

---
+++ 03.Conditional-Statements-Advanced-Lab/02.WeekendorWorking Day/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _02.WeekendorWorking_Day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "Monday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+
+                    }
+                case "Tuesday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+
+                    }
+                case "Wednesday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+
+                    }
+                case "Thursday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+
+                    }
+                case "Friday":
+                    {
+                        Console.WriteLine("Working day");
+                        break;
+
+                    }
+                case "Saturday":
+                    {
+                        Console.WriteLine("Weekend");
+                        break;
+                    }
+                case "Sunday":
+                    {
+                        Console.WriteLine("Weekend");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Error");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ 03.Conditional-Statements-Advanced-Lab/03.AnimalType/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _03.AnimalType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            switch (animal)
+            {
+                case "dog":
+                    {
+                        Console.WriteLine("mammal");
+                        break;
+                    }
+                case "crocodile":
+                    {
+                        Console.WriteLine("reptile");
+                        break;
+                    } 
+                case "tortoise":
+                    {
+                        Console.WriteLine("reptile");
+                        break;
+                    } 
+                case "snake":
+                    {
+                        Console.WriteLine("reptile");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("unknown");
+                        break;
+                    }
+            }
+        }
+    }
+}
